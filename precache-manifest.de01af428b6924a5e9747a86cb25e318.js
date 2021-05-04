9d3e6f2ccb70a5c46f7f06c778e62f6037af1303
Payal1414 <ghosalkarpayal@gmail.com>
2021-05-04 16:01:24 +0530

    Updates

---
+++ precache-manifest.de01af428b6924a5e9747a86cb25e318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3995d9784691efb98c8f",
+    "url": "/rjew/static/css/main.1bb4be3e.chunk.css"
+  },
+  {
+    "revision": "3995d9784691efb98c8f",
+    "url": "/rjew/static/js/main.1b159b5e.chunk.js"
+  },
+  {
+    "revision": "5966e10980731e34cab2",
+    "url": "/rjew/static/js/runtime~main.95f912b8.js"
+  },
+  {
+    "revision": "1f36666d3942d25c4014",
+    "url": "/rjew/static/js/2.69a5caef.chunk.js"
+  },
+  {
+    "revision": "4290bc807f898ade8bc9a4296735ab10",
+    "url": "/rjew/static/media/newLogo.4290bc80.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/rjew/static/media/shopping-bag.09e6f5e7.svg"
+  },
+  {
+    "revision": "30998248d83281827d981ec9f24ff47b",
+    "url": "/rjew/index.html"
+  }
+];
